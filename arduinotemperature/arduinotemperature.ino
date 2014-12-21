8c6058e720e5adaecf7499ad00a54df9ad372aa5
Jim Wallace <jim@extroverteddeveloper.com>
2014-12-21 14:27:41 -0500

    add ardino A/D i2c code to repo

---
+++ arduinotemperature/arduinotemperature.ino
@@ -0,0 +1,73 @@
+/*
+  AnalogReadSerial
+  Reads an analog input on pin 0, prints the result to the serial monitor.
+  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
+ 
+ This example code is in the public domain.
+ */
+
+#include <Wire.h>
+#define ADDRESS 0x60
+
+const int temperaturePin = 0;
+static int temperature = 0;
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  Wire.begin(ADDRESS);
+  Wire.onRequest(requestEvent);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  float voltage, degreesC, degreesF;
+  
+  voltage = getVoltage(temperaturePin);
+  degreesC = (voltage - 0.5) * 100.0;
+  degreesF = degreesC * (9.0/5.0) + 32.0;
+  temperature = (int)degreesF;
+  
+  // print out the value you read:  
+  delay(100);        // delay in between reads for stability
+}
+
+void requestEvent() { 
+  Wire.write(temperature);
+}  
+
+float getVoltage(int pin)
+{
+  // This function has one input parameter, the analog pin number
+  // to read. You might notice that this function does not have
+  // "void" in front of it; this is because it returns a floating-
+  // point value, which is the true voltage on that pin (0 to 5V).
+  
+  // You can write your own functions that take in parameters
+  // and return values. Here's how:
+  
+    // To take in parameters, put their type and name in the
+    // parenthesis after the function name (see above). You can
+    // have multiple parameters, separated with commas.
+    
+    // To return a value, put the type BEFORE the function name
+    // (see "float", above), and use a return() statement in your code
+    // to actually return the value (see below).
+  
+    // If you don't need to get any parameters, you can just put
+    // "()" after the function name.
+  
+    // If you don't need to return a value, just write "void" before
+    // the function name.
+
+  // Here's the return statement for this function. We're doing
+  // all the math we need to do within this statement:
+  
+  return (analogRead(pin) * 0.004882814);
+  
+  // This equation converts the 0 to 1023 value that analogRead()
+  // returns, into a 0.0 to 5.0 value that is the true voltage
+  // being read at that pin.
+}
+
